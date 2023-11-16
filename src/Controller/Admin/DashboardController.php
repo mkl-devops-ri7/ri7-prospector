a554149d22241f9154344a09dbcb379c3c75d2b5
laurenttarallo tarallo <laurent.tarallo@orange.fr>
2023-11-16 15:48:09 +0000

    add crud company

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,8 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Company;
-use App\Entity\Action;
 use App\Entity\Notification;
+use App\Entity\Action;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
