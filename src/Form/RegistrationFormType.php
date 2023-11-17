7e27076197915d2e3ae9c291b3bd490d0a12bfcf
remystreich <127842002+remystreich@users.noreply.github.com>
2023-11-17 14:25:10 +0100

    registration form (#90)
    Co-authored-by: Alan <62551143+alan-vle@users.noreply.github.com>

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,12 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -18,22 +18,61 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName', TextType::class)
-            ->add('lastName', TextType::class)
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail',
+                'label_attr' => [
+                    'class' => 'block text-sm font-medium text-gray-700',
+                ],
+                'attr' => [
+                    'class' => 'mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm', // Ajout de classes au champ lui-même
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'label_attr' => [
+                    'class' => 'block text-sm font-medium text-gray-700',
+                ],
+                'attr' => [
+                    'class' => 'mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm', // Ajout de classes au champ lui-même
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'block text-sm font-medium text-gray-700',
+                ],
+                'attr' => [
+                    'class' => ' mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm', // Ajout de classes au champ lui-même
+                ],
+            ])
+
+            ->add('plainPassword', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'type' => PasswordType::class,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'required' => true,
+                'attr' => [
+                    'class' => 'flex',
+                ],
+                'first_options' => [
+                    'label' => 'Mot de Passe',
+                    'label_attr' => [
+                        'class' => 'block text-sm font-medium text-gray-700 col-6',
+                    ],
+                    'attr' => [
+                        'class' => 'mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation de mot de passe',
+                    'label_attr' => [
+                        'class' => 'block text-sm font-medium text-gray-700 col-6',
+                    ],
+                    'attr' => [
+                        'class' => 'mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm',
+                    ],
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -46,6 +85,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+
         ;
     }
 
@@ -53,6 +93,7 @@ class RegistrationFormType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'attr' => ['class' => 'mt-8 grid grid-cols-6 gap-6'],
         ]);
     }
 }
