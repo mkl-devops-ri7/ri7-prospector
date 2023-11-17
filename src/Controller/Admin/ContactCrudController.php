8ab186f8a356a8fe5284c720449701bb164454aa
Bontano <hassen.tom07@gmail.com>
2023-11-17 15:42:56 +0100

    -([^-]+)-(.+)/\2:

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -4,8 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Contact;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ContactCrudController extends AbstractCrudController
@@ -17,13 +17,14 @@ class ContactCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield IdField::new('id');
-        yield  TextField::new('email');
-        yield  TextField::new('job');
-        yield  TextField::new('firstname');
-        yield  TextField::new('lastname');
-        yield  TextField::new('linkedinProfilUrl');
-        yield  TextField::new('phoneNumber');
-        yield  TextEditorField::new('description');
+        yield IdField::new('id')->hideOnForm();
+        yield TextField::new('email');
+        yield TextField::new('job');
+        yield TextField::new('firstname');
+        yield TextField::new('lastname');
+        yield TextField::new('linkedinProfilUrl');
+        yield TextField::new('phoneNumber');
+        yield AssociationField::new('prospections')->hideOnForm();
+        yield AssociationField::new('company');
     }
 }
