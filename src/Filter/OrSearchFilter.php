7e5825e5f16a7a53ffdab99d3384346607a62da1
MKL DEVOPS <88685078+mkldevops@users.noreply.github.com>
2023-12-11 00:06:05 +0100

    feat: #164 inspection show implement auto completion on input contact (#169)

---
+++ src/Filter/OrSearchFilter.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filter;
+
+use ApiPlatform\Doctrine\Orm\Filter\AbstractFilter;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\PropertyInfo\Type;
+
+final class OrSearchFilter extends AbstractFilter
+{
+    /**
+     * @param array<string> $context
+     * @param string        $value
+     */
+    protected function filterProperty(
+        string $property,
+        $value,
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        Operation $operation = null,
+        array $context = []
+    ): void {
+        // Otherwise filter is applied to order and page as well
+        if (
+            !$this->isPropertyEnabled($property, $resourceClass)
+            || !$this->isPropertyMapped($property, $resourceClass)
+        ) {
+            return;
+        }
+
+        $parameterName = $queryNameGenerator->generateParameterName($property); // Generate a unique parameter name to avoid collisions with other filters
+        $queryBuilder
+            ->orWhere("o.{$property} LIKE :{$parameterName}")
+            ->setParameter($parameterName, "%$value%");
+    }
+
+    /**
+     * This function is only used to hook in documentation generators (supported by Swagger and Hydra).
+     *
+     * @return array<string, array<string, array<string, bool|string>|bool|int|string>>
+     */
+    public function getDescription(string $resourceClass): array
+    {
+        if (!$this->properties) {
+            return [];
+        }
+
+        $description = [];
+        foreach ($this->properties as $property => $strategy) {
+            $description["or_search_$property"] = [
+                'property' => $property,
+                'type' => Type::BUILTIN_TYPE_STRING,
+                'required' => false,
+                'description' => 'Filter using a regex. This will appear in the OpenApi documentation!',
+                'openapi' => [
+                    'example' => 'Custom example that will be in the documentation and be the default value of the sandbox',
+                    'allowReserved' => false, // if true, query parameters will be not percent-encoded
+                    'allowEmptyValue' => true,
+                    'explode' => false, // to be true, the type must be Type::BUILTIN_TYPE_ARRAY, ?product=blue,green will be ?product=blue&product=green
+                ],
+            ];
+        }
+
+        return $description;
+    }
+}
